5e748196bdbbfddc859de16b0bbc7e86633872e0
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-04 13:50:52 -0500

    Add more methods
    Added more methods to be implemented later.

---
+++ src/main/java/org/example/Role.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public interface Role {
+    public void characterRole();
+    public int getStrengthScore();
+    public int getCraftScore();
+    public int getHealthPoints();
+}
