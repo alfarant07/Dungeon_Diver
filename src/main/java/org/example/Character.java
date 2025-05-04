a4dbc190c244d443a63ec262226ccb02d6284868
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-04 13:52:08 -0500

    Added private fields and constructor
    Implemented private fields that will be used by future game logic.
    Setting these fields using our interface methods since stats
    will vary on class.

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -1,5 +1,15 @@
 package org.example;
 
 public class Character {
+    private int strengthScore;
+    private int craftScore;
+    private int healthPoints;
+    Role role;
+    public Character(Role role){
+        this.role=role;
+        this.craftScore=role.getCraftScore();
+        this.healthPoints=role.getHealthPoints();
+        this.strengthScore= role.getStrengthScore();
+    }
 
 }
