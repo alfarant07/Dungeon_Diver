8fe65806d354d44f2bc54e5fd2ec710de3b6145a
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-05 12:29:32 -0500

    Implemented MagicWand class
    Our MagicWand class implements item interface
    and improves the craft score by a base of 2+ the items
    rarity.

---
+++ src/main/java/org/example/MagicWand.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+
+public class MagicWand implements Items{
+    private int craftScore = 2;
+    private Rarity rarity = Rarity.COMMON;
+    public MagicWand(){
+
+    }
+    public MagicWand(Rarity rarity){
+        this.craftScore += rarity.getItemModifier();
+    }
+
+    @Override
+    public int getValue() {
+        return craftScore;
+    }
+}
+
