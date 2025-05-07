e4ce1e7e50189f3cee2d18fbf6c0bfeea26181c2
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-06 21:51:34 -0500

    Implemented DarkMage Monster
    Implemented a DarkMage monster with random craft and healthPoints.

---
+++ src/main/java/org/example/DarkMage.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Random;
+
+public class DarkMage extends Monster{
+    private int strengthScore =0;
+    private int craftScore;
+    private int healthPoints;
+    Random r = new Random();
+    public DarkMage(){
+        this.craftScore= r.nextInt(20);
+        this.healthPoints=r.nextInt(15);
+
+    }
+
+    @Override
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    @Override
+    public int getCraftScore() {
+        return craftScore;
+    }
+
+    @Override
+    public int getStrengthScore() {
+        return strengthScore;
+    }
+
+    public int deductHealthPoints(Character player) {
+        int characterAttack = (r.nextInt(6)+1) + player.getCraftScore();
+        int monsterAttack = (r.nextInt(6)+1) + this.getCraftScore();
+        return characterAttack-monsterAttack;
+    }
+    public void setHealthPoints(int adjustedHP){
+        this.healthPoints-=adjustedHP;
+    }
+}
