07b2a843c47357dbfa6d3db6bb714126b4914d6c
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-08 23:15:31 -0500

    Add addition methods for stats
    Implemented addition methods required by Item interface to augment
    stats.

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -6,23 +6,41 @@ public class Character {
     private int strengthScore;
     private int craftScore;
     private int healthPoints;
-    private int armorScore;
-    private ArrayList<Items> inventory = new ArrayList<>();
-    private int leftHandItem=0;
-    private int rightHandItem=0;
-    Role role;
-    public Character(Role role){
-        this.role=role;
-        this.craftScore=role.getCraftScore();
-        this.healthPoints=role.getHealthPoints();
-        this.strengthScore= role.getStrengthScore();
+    private String playerName;
+    public Character(int craftScore, int strengthScore, int healthPoints,String playerName) {
+        this.craftScore = 0;
+        this.healthPoints = 0;
+        this.strengthScore = 0;
+        this.playerName = playerName;
     }
-    public Character(Role role,Items leftHand){
-        if(leftHand instanceof Shield) healthPoints+=leftHand.getValue();
-        if(leftHand instanceof Sword) strengthScore += leftHand.getValue();
-        if(leftHand instanceof MagicWand) craftScore += leftHand.getValue();
+    public int getCraftScore() {
+        return craftScore;
     }
 
+    public int getStrengthScore() {
+        return strengthScore;
+    }
 
+    public int getHealthPoints() {
+        return healthPoints;
+    }
 
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+    public void addStrengthScore(int modifier){
+        this.strengthScore+=modifier;
+
+    }
+    public void addCraftScore(int modifier){
+        this.craftScore+=modifier;
+
+    }
+    public void addHealthpoints(int modifier){
+        this.healthPoints+=modifier;
+    }
 }
