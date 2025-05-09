6968495002fd7ab084a4a61e34f898d0179c7398
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-08 22:08:56 -0500

    Prepping for merge conflict
    # Conflicts:
    #	src/main/java/org/example/Character.java

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -6,14 +6,6 @@ public class Character {
     private int strengthScore;
     private int craftScore;
     private int healthPoints;
-<<<<<<< HEAD
-    private String playerName;
-    public Character(int craftScore, int strengthScore, int healthPoints,String playerName){
-        this.craftScore=0;
-        this.healthPoints=0;
-        this.strengthScore=0;
-        this.playerName =playerName;
-=======
     private int armorScore;
     private ArrayList<Items> inventory = new ArrayList<>();
     private int leftHandItem=0;
@@ -24,7 +16,6 @@ public class Character {
         this.craftScore=role.getCraftScore();
         this.healthPoints=role.getHealthPoints();
         this.strengthScore= role.getStrengthScore();
->>>>>>> 532fc85 (Implemented inventory and armor score field.)
     }
     public Character(Role role,Items leftHand){
         if(leftHand instanceof Shield) healthPoints+=leftHand.getValue();
@@ -34,24 +25,4 @@ public class Character {
 
 
 
-
-    public int getCraftScore() {
-        return craftScore;
-    }
-
-    public int getStrengthScore() {
-        return strengthScore;
-    }
-
-    public int getHealthPoints() {
-        return healthPoints;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public void setHealthPoints(int healthPoints) {
-        this.healthPoints = healthPoints;
-    }
 }
