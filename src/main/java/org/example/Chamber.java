6d74ffb0d739b98b2bc91c0e93bf5ae0f9496fbd
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-09 15:44:26 -0500

    Add remove monster methods
    Implemented remove monster method by removing our chamber from the
    hashmap of blockedDoors.

--- src/main/java/org/example/Chamber.java
+++ src/main/java/org/example/Chamber.java
@@ -4,27 +4,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Chamber {
-    private ArrayList<Items> itemsInChamber;
+    private ArrayList<Items> itemsInChamber = new ArrayList<>();
     private int numDoors;
     private ArrayList<Chamber> adj = new ArrayList<>();
     private HashMap<Monster, Chamber> blockedDoors = new HashMap<>();
 
-    public Chamber(){
-    }
-    public Chamber(Items item){
-        itemsInChamber.add(item);
-    }
-    public ArrayList<Items> getItems(){
-        return itemsInChamber;
-    }
-    public void addEdge(Chamber neighbor){
-        this.adj.add(neighbor);
-    }
-    public void addMonster(Monster monster,Chamber destination){
-        this.blockedDoors.put(monster,destination);
-    }
-    public ArrayList<Chamber> getDoors(){
-        return adj;
+    public Chamber() {
     }
 
+    public Chamber(Items item) {
+        itemsInChamber.add(item);
+    }
+
+    public ArrayList<Items> getItems() {
+        return itemsInChamber;
+    }
+
+    public void addEdge(Chamber neighbor) {
+        this.adj.add(neighbor);
+    }
+
+    public void addMonster(Monster monster, Chamber destination) {
+        this.blockedDoors.put(monster, destination);
+    }
+    public void removeMonster(Chamber destination){
+        this.blockedDoors.remove(destination);
+    }
+
+    public ArrayList<Chamber> getDoors() {
+        return adj;
+    }
+    public HashMap<Monster,Chamber> getBlockedDoors(){
+        return blockedDoors;
+    }
+    public boolean monsterBlockingDoor(Chamber destination){
+        return this.getBlockedDoors().containsValue(destination);
+    }
 }
