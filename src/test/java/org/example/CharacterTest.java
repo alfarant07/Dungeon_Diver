c0270b0f9a9e912853b41614287ba26c5d69abfd
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-08 21:55:59 -0500

    Implemented test for Character constructor.

---
+++ src/test/java/org/example/CharacterTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterTest {
+
+    @org.junit.jupiter.api.Test
+    void wizardConstructor() {
+        Character character = new Wizard("Caraxes");
+        assertEquals(character.getCraftScore(),16);
+        assertEquals(character.getStrengthScore(),7);
+        assertEquals(character.getHealthPoints(),11);
+        assertEquals(character.getPlayerName(),"Caraxes");
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void warriorConstructor() {
+        Character character = new Warrior("Balerion");
+        assertEquals(character.getCraftScore(),6);
+        assertEquals(character.getStrengthScore(),16);
+        assertEquals(character.getHealthPoints(),15);
+        assertEquals(character.getPlayerName(),"Balerion");
+    }
+}
