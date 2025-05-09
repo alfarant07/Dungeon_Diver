d2eb07169698d2bcef5325ad26ddbe213c051736
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-09 16:05:58 -0500

    Refactored fight execute
    Readjusted fight execute logic to remove the chamber from the blocked
    doors hashmap if monster hp reaches below zero

--- src/main/java/org/example/Fight.java
+++ src/main/java/org/example/Fight.java
@@ -3,18 +3,23 @@ package org.example;
 public class Fight extends Action {
     private Character player;
     private Monster monster;
-    public Fight(Monster monster, Character player){
+    private Chamber destination;
+    private Dungeon d;
+
+    public Fight(Monster monster, Character player, Chamber destination) {
         super("Fight Monster:" + monster.getName());
         this.player = player;
-        this.monster=monster;
+        this.monster = monster;
+        destination = destination;
     }
-    public void execute(){
-        int damageTaken = this.monster.deductHealthPoints(this.player);
-        if(damageTaken>0){
-            this.monster.setHealthPoints(damageTaken);
-        }else{
-            this.player.setHealthPoints(damageTaken);
+        public void execute () {
+            int damageTaken = this.monster.deductHealthPoints(this.player);
+            if (damageTaken > 0) {
+                this.monster.setHealthPoints(damageTaken);
+            } else {
+                this.player.setHealthPoints(damageTaken);
+            }
+            if (monster.getHealthPoints() < 0) d.getCurrentChamber().removeMonster(destination);
         }
 
-    }
 }
