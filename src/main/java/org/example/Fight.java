d58b2998fdacba9c9d65f0b3caeab11c9bb1ef4c
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-09 20:52:21 -0500

    Fixed bug
    Never assigned this.destination to this preventing removal of monster
    from hashmap

--- src/main/java/org/example/Fight.java
+++ src/main/java/org/example/Fight.java
@@ -1,16 +1,19 @@
 package org.example;
 
+import javax.annotation.processing.SupportedSourceVersion;
+
 public class Fight extends Action {
     private Character player;
     private Monster monster;
     private Chamber destination;
     private Dungeon d;
 
-    public Fight(Monster monster, Character player, Chamber destination) {
+    public Fight(Monster monster, Character player, Chamber destination,Dungeon dungeon) {
         super("Fight Monster:" + monster.getName());
         this.player = player;
         this.monster = monster;
-        destination = destination;
+        this.destination = destination;
+        d=dungeon;
     }
         public void execute () {
             int damageTaken = this.monster.deductHealthPoints(this.player);
@@ -19,7 +22,12 @@ public class Fight extends Action {
             } else {
                 this.player.setHealthPoints(damageTaken);
             }
-            if (monster.getHealthPoints() <= 0) d.getCurrentChamber().removeMonster(destination);
+            if (monster.getHealthPoints() <= 0) {
+                System.out.println(d.getCurrentChamber().getBlockedDoors().entrySet());
+                d.getCurrentChamber().removeMonster(destination);
+                System.out.println(d.getCurrentChamber().getBlockedDoors().entrySet());
+                System.out.println("Monster has been slain");
+            }
         }
 
 }
