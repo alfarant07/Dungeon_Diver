d1b8f75afb2c9e83cf5d47e4dbfb173e50d55dda
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-10 01:09:18 -0500

    Fixed bug
    returning this.values to avoid possible errors.

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -8,44 +8,59 @@ public class Character {
     private int healthPoints;
     private String playerName;
     private ArrayList<Items> inventory = new ArrayList<>();
+    private Items leftHand;
+    private Items rightHand;
 
     public Character(int craftScore, int strengthScore, int healthPoints,String playerName) {
-        this.craftScore = 0;
-        this.healthPoints = 0;
-        this.strengthScore = 0;
+        this.craftScore = craftScore;
+        this.healthPoints = healthPoints;
+        this.strengthScore = strengthScore;
         this.playerName = playerName;
     }
     public int getCraftScore() {
-        return craftScore;
+        return this.craftScore;
     }
 
     public int getStrengthScore() {
-        return strengthScore;
+        return this.strengthScore;
     }
 
     public int getHealthPoints() {
-        return healthPoints;
+        return this.healthPoints;
     }
 
     public String getPlayerName() {
-        return playerName;
+        return this.playerName;
     }
 
     public void setHealthPoints(int healthPoints) {
         this.healthPoints = healthPoints;
     }
     public void addStrengthScore(int modifier){
-        this.strengthScore+=modifier;
+        this.strengthScore= this.strengthScore+modifier;
 
     }
     public void addCraftScore(int modifier){
-        this.craftScore+=modifier;
+        this.craftScore= this.craftScore + modifier;
 
     }
     public void addHealthpoints(int modifier){
-        this.healthPoints+=modifier;
+        this.healthPoints=this.healthPoints+modifier;
     }
     public void addItem(Items items){
         inventory.add(items);
+        if(leftHand ==null){
+            equipLeft(items);
+        } else if (rightHand==null) {
+            equipRight(items);
+        }
     }
-}
+    public void equipLeft(Items item){
+        this.leftHand = item;
+        item.applyModifier(this);
+    }
+    public void equipRight(Items item){
+        this.rightHand = item;
+        item.applyModifier(this);
+    }
+    }
--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -3,18 +3,18 @@ package org.example;
 public class Game {
     public static void main(String[]args){
         Chamber[] chambers = new Chamber[]{
-                new Chamber(),
+                new Chamber(new Shield()),
                 new Chamber(),
                 new Chamber(),
                 new Chamber(),
                 new Chamber()
         };
-        Door.connect(chambers[0],chambers[1]);
+        Door.connect(chambers[0],chambers[1],new DarkMage());
         Door.connect(chambers[1],chambers[2]);
         Door.connect(chambers[2],chambers[3]);
         Door.connect(chambers[3],chambers[4]);
         Character player = new Wizard("Gandalf");
-        Dungeon d = new Dungeon(chambers[0],chambers[4],player);
+        Dungeon d = new Dungeon(chambers[0],chambers[2],player);
         TextUI ui = new TextUI();
         ui.play(d);
 
