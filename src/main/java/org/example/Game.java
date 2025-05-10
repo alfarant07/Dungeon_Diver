9f3e4917097546c1cfc554f2f8f158878638b765
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-10 13:12:35 -0500

    Rewrote game
    Rewrote game class to be similar to the given game class.

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -10,7 +10,8 @@ public class Game {
                 new Chamber()
         };
         Door.connect(chambers[0],chambers[1],new DarkMage());
-        Door.connect(chambers[1],chambers[2]);
+        Door.connect(chambers[0],chambers[3]);
+        Door.connect(chambers[1], chambers[2], new CustomMonster("Dragon",0,10,5));
         Door.connect(chambers[2],chambers[3]);
         Door.connect(chambers[3],chambers[4]);
         Character player = new Wizard("Gandalf");
