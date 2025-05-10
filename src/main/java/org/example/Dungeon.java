568da8f93854738c1c9599e6ea79493107870b65
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-10 13:10:13 -0500

    Add isValid
    Implemented isValid which is a dfs algorithm to check if our Dungeon has
    an exit.

--- src/main/java/org/example/Dungeon.java
+++ src/main/java/org/example/Dungeon.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Dungeon {
     private int doors;
@@ -8,40 +8,63 @@ public class Dungeon {
     private Chamber chamberExit;
     private Chamber currentChamber;
     private Character player;
-    private ArrayList<Action> actionsAvailable =new ArrayList<>();
+    private ArrayList<Action> actionsAvailable = new ArrayList<>();
 
-    public Dungeon(Chamber entrance, Chamber exit, Character player){
-        this.chamberEntrance=entrance;
+    public Dungeon(Chamber entrance, Chamber exit, Character player) {
+        this.chamberEntrance = entrance;
         this.currentChamber = chamberEntrance;
-        this.chamberExit=exit;
-        this.player=player;
+        this.chamberExit = exit;
+        this.player = player;
     }
-    public Chamber getCurrentChamber(){
+
+    public Chamber getCurrentChamber() {
         return currentChamber;
     }
-    public boolean isFinished(){
-        if(currentChamber == chamberExit || player.getHealthPoints()<0) return true;
+
+    public boolean isFinished() {
+        if (currentChamber == chamberExit || player.getHealthPoints() < 0) return true;
         return false;
     }
-    public void setCurrentChamber(Chamber destination){
+
+    public void setCurrentChamber(Chamber destination) {
         this.currentChamber = destination;
     }
-    public ArrayList<Action> getActions(){
+
+    public ArrayList<Action> getActions() {
         actionsAvailable.clear();
-        for(Items items:currentChamber.getItems() ){
-            Action item = new Pick(player,items,currentChamber);
+        for (Items items : currentChamber.getItems()) {
+            Action item = new Pick(player, items, currentChamber);
             actionsAvailable.add(item);
         }
-        for(Chamber chamber:currentChamber.getDoors() ){
-            Action doors = new Move(this,chamber);
+        for (Chamber chamber : currentChamber.getDoors()) {
+            Action doors = new Move(this, chamber);
             actionsAvailable.add(doors);
         }
         //https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html#forEach-java.util.function.BiConsumer-
         currentChamber.getBlockedDoors().forEach((chamber, monster) -> {
-            if(monster.getHealthPoints()>0) {
+            if (monster.getHealthPoints() > 0) {
                 actionsAvailable.add(new Fight(monster, player, chamber, this));
             }
         });
         return actionsAvailable;
     }
+
+    public boolean validDungeon(Chamber chamberEntrance, Chamber chamberExit) {
+        Set<Chamber> visited = new HashSet<>();
+        Stack<Chamber> stack = new Stack<>();
+        stack.push(chamberEntrance);
+        while(!stack.isEmpty()){
+            Chamber current = stack.pop();
+            visited.add(current);
+            for(Chamber neighbor: current.getDoors()){
+                if(!visited.contains(neighbor)){
+                    stack.push(neighbor);
+                }
+            }
+            }
+        if(!visited.contains(chamberExit))return false;
+        return true;
+
+    }
 }
+
