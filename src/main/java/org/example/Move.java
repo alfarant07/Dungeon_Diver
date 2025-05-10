a8fa8d0246e241f3ede1256b362a10cafcd86e9e
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-09 19:48:38 -0500

    Add better string formatting
    Implemented better string formatting

--- src/main/java/org/example/Move.java
+++ src/main/java/org/example/Move.java
@@ -4,12 +4,12 @@ public class Move extends Action{
     private Dungeon d;
     private Chamber destination;
     public Move(Dungeon d, Chamber destination){
-        super("Move to" + destination);
+        super("Move to:" + destination.getChamberName());
         this.d = d;
         this.destination = destination;
     }
     public void execute(){
-        if(!destination.monsterBlockingDoor(destination)) {
+        if(!d.getCurrentChamber().monsterBlockingDoor(destination)) {
             d.setCurrentChamber(destination);
         }else{
             System.out.print("Monster blocking the path");
