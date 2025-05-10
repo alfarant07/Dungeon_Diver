d17c88d7310c22b2ae1093ce07f14b52f0dae7cb
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-09 19:49:54 -0500

    Add chamber and item parameter
    Implemented currentChamber and item parameter to remove item from
    chamber item list.

--- src/main/java/org/example/Pick.java
+++ src/main/java/org/example/Pick.java
@@ -3,12 +3,15 @@ package org.example;
 public class Pick extends Action{
     private Character player;
     private Items item;
-    public Pick(Character player, Items item){
-        super("Pick up Item"+ Items.getName());
-        player = player;
+    private Chamber current;
+    public Pick(Character player, Items pickedUpItem,Chamber currentChamber){
+        super("Pick up Item:"+ Items.getName());
+        this.item = pickedUpItem;
+        this.player = player;
+        current=currentChamber;
     }
     public void execute(){
         player.addItem(item);
-
+        current.removeItem(item);
     }
 }
