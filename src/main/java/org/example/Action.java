b6c8dd84476ce772df2a80410c26e41e24701920
Anthony Alfaro <alfaro2005@yahoo.com>
2025-05-10 13:08:02 -0500

    Added javadocs

--- src/main/java/org/example/Action.java
+++ src/main/java/org/example/Action.java
@@ -1,11 +1,30 @@
 package org.example;
 
+/**
+ * Abstract class of to be used by possible actions
+ * prompt meant for TextUI format
+ */
+
 public abstract class Action {
     private String prompt;
+
+    /**
+     * we construct an Action with a prompt for nice output
+     * @param prompt subclasses pass prompt on their respective action
+     */
     public Action(String prompt){
         this.prompt = prompt;
     }
+
+    /**
+     * execute implemented by subclasses to do their effect
+     */
     public abstract void execute();
+
+    /**
+     *
+     * @return string representation of prompt and not javas base toString
+     */
     public String toString(){
         return prompt;
     }
